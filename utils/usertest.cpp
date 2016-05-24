d89b42cdc4af0a9f7e4b42b43f0a705258ee2e4c
HerrLingvistik <gusha495@studen.liu.se>
2016-05-24 11:22:26 +0200

    started working on user tests

---
+++ utils/usertest.cpp
@@ -0,0 +1,14 @@
+#include "common.h"
+#include "usertest.h"
+
+void task1(std::string &resultString, int posX, int posY, float texture[][H]){
+	resultString = resultString + to_string(texture[posX][posY]);
+}
+
+void task2(std::string &resultString){
+
+}
+
+void task3(std::string &resultString){
+
+}
---
+++ utils/usertest.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string.h>
+
+void task1(std::string &resultString, int posX, int posY, float texture[][H]);
+
+void task2(std::string &resultString);
+
+void task3(std::string &resultString);
